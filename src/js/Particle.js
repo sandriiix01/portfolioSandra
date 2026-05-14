4a4e47095c7fedca3d036fc0052a51e3a822006c
Javier Roca <javierroca.dev@gmail.com>
2026-05-15 01:18:42 +0200

    Particles

---
+++ src/js/Particle.js
@@ -0,0 +1,195 @@
+particlesJS("particles-js", {
+  particles: {
+    number: {
+      value: 200,
+
+      density: {
+        enable: true,
+
+        value_area: 800,
+      },
+    },
+
+    color: {
+      value: "#ffffff",
+    },
+
+    shape: {
+      type: "circle",
+    },
+
+    opacity: {
+      value: 0.35,
+
+      random: true,
+    },
+
+    size: {
+      value: 2,
+
+      random: false,
+
+      anim: {
+        enable: false,
+      },
+    },
+
+    line_linked: {
+      enable: false,
+
+      opacity: 0.2,
+    },
+
+    move: {
+      enable: true,
+
+      speed: 1,
+
+      direction: "none",
+
+      random: true,
+
+      straight: false,
+
+      out_mode: "out",
+
+      bounce: false,
+
+      attract: {
+        enable: false,
+
+        rotateX: 600,
+
+        rotateY: 1200,
+      },
+    },
+  },
+
+  interactivity: {
+    detect_on: "window",
+
+    events: {
+      onhover: {
+        enable: true,
+
+        mode: "bubble",
+      },
+
+      onclick: {
+        enable: false,
+      },
+
+      resize: true,
+    },
+
+    modes: {
+      bubble: {
+        distance: 200,
+
+        size: 2.2,
+
+        duration: 2,
+
+        opacity: 1,
+
+        speed: 3,
+      },
+    },
+  },
+
+  retina_detect: true,
+});
+
+// particlesJS("particles-js", {
+//   particles: {
+//     number: {
+//       value: 200,
+
+//       density: {
+//         enable: true,
+
+//         value_area: 800,
+//       },
+//     },
+
+//     color: {
+//       value: "#ffffff",
+//     },
+
+//     shape: {
+//       type: "circle",
+//     },
+
+//     opacity: {
+//       value: 0.35,
+
+//       random: true,
+//     },
+
+//     size: {
+//       value: 2,
+
+//       random: false,
+
+//       anim: {
+//         enable: false,
+//       },
+//     },
+
+//     line_linked: {
+//       enable: false,
+
+//       opacity: 0.2,
+//     },
+
+//     move: {
+//       enable: true,
+
+//       speed: 1,
+
+//       direction: "none",
+
+//       random: true,
+
+//       straight: false,
+
+//       out_mode: "out",
+
+//       bounce: false,
+
+//       attract: {
+//         enable: false,
+
+//         rotateX: 600,
+
+//         rotateY: 1200,
+//       },
+//     },
+//   },
+
+//   interactivity: {
+//     detect_on: "window",
+
+//     events: {
+//       onhover: {
+//         enable: true,
+//         mode: "repulse",
+//       },
+
+//       onclick: {
+//         enable: false,
+//       },
+
+//       resize: true,
+//     },
+
+//     modes: {
+//       repulse: {
+//         distance: 100, // Radio de distancia en píxeles
+//         duration: 0.4,
+//       },
+//     },
+//   },
+
+//   retina_detect: true,
+// });
