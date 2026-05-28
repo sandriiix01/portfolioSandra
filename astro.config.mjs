8f8467891a38c8073f96d22befec586ca65a74b6
Javier Roca <javierroca.dev@gmail.com>
2026-05-28 22:15:01 +0200

    seo

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,17 +1,18 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
-import tailwindcss from '@tailwindcss/vite';
+import tailwindcss from "@tailwindcss/vite";
 
-import sanity from '@sanity/astro';
+import sanity from "@sanity/astro";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://portfolio-sandra-nine.vercel.app/",
   vite: {
-    plugins: [tailwindcss()]
+    plugins: [tailwindcss()],
   },
 
-integrations: [
+  integrations: [
     sanity({
       projectId: "csd7x6wg",
       dataset: "production",
